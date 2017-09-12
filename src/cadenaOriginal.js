a9bbd12e501017da2a0dbd61ca056c41b28b3a25
Alan Rodriguez <alanrodriguezromero@gmail.com>
2017-09-12 16:24:14 -0500

    Add jsdoc to cadenaOriginal functions

--- src/cadenaOriginal.js
+++ src/cadenaOriginal.js
@@ -1,8 +1,16 @@
 import libxslt from 'libxslt'
 import path from 'path'
 import fs from 'fs'
+
 const cadenaPath = path.join('src', 'xslt', 'cadenaoriginal_3_3.xslt')
 
+/**
+ * Converts a callback to a promise, used for async/await
+ *
+ * @function
+ * @param {...*} args - Arguments
+ * @return {Promise} Callback converted to promise
+ */
 function callbackToPromise (method, ...args) {
   return new Promise(function (resolve, reject) {
     return method(...args, function (err, result) {
@@ -12,6 +20,13 @@ function callbackToPromise (method, ...args) {
 }
 
 export default {
+  /**
+   * Generate a Cadena Original from a given XML string
+   *
+   * @async
+   * @param  {string}  facturaXML - The XML string of a 3.3 factura
+   * @return {Promise<string>} Cadena Original string result
+   */
   generaCadena: async (facturaXML) => {
     if (!facturaXML) return false
     try {
@@ -26,6 +41,13 @@ export default {
       return false
     }
   },
+  /**
+   * Generate a Cadena Original from a given XML file path
+   *
+   * @async
+   * @param  {string}  facturaPath - Path to the .xml file
+   * @return {Promise<string>} Cadena Original string result
+   */
   generaCadenaFile: async (facturaPath) => {
     if (!facturaPath) return false
     try {
